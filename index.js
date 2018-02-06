eabf618afecc14c15b69f38a87d93065e6a4a3c1
anhez <anhtd.dev@gmail.com>
2018-02-06 16:29:50 +0700

    a1
    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s", host, port)
+});
+
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
